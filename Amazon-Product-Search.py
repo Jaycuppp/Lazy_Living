bf3f3a3b31920f87356fe0f4b2655fb4c784d949
Jakob <108171765+Jaycuppp@users.noreply.github.com>
2022-12-26 01:30:18 -0800

    Update Amazon-Product-Search.py

--- Amazon-Product-Search.py
+++ Amazon-Product-Search.py
@@ -1,25 +1,22 @@
-from asyncio import wait_for
-from xml.dom.minidom import Element
 import selenium
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 
-# Selenium Setup
+
+#Initial Setup
 path = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(path)
-driver.get('https://www.amazon.com/?tag=amazusnavi-20&hvadid=381891677693&hvpos=&hvnetw=g&hvrand=12793925684753999037&hvpone=&hvptwo=&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9031162&hvtargid=kwd-49491430&ref=pd_sl_67w2s0guum_e&hydadcr=28882_11845412&gclid=Cj0KCQiAwJWdBhCYARIsAJc4idCn6lvaH6_qYovLmXdDFzSpKX9adqiMBUssezNFtVtzKjkKYfTOUw4aAr0FEALw_wcB')
-wait = WebDriverWait(driver, 0)
-original_window = driver.current_window_handle
-actions = ActionChains(driver)
+driver.get('https://www.amazon.com/?tag=amazusnavi-20&hvadid=381823327672&hvpos=&hvnetw=g&hvrand=15369903253882284441&hvpone=&hvptwo=&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9031162&hvtargid=kwd-10573980&ref=pd_sl_7j18redljs_e&hydadcr=28883_11845445&gclid=CjwKCAjwmJeYBhAwEiwAXlg0ATpf1Z-JL2kpyEl7yRZRhGWjvKzG3tVkde1CAQCiNZ9JaYRHyJXjjRoCf5wQAvD_BwE')
 
+# GPU Keystroke Input
+Search_Button = driver.find_element(By.XPATH, '//*[@id="twotabsearchtextbox"]')
+# Change this to any product you want
+Search_Button.send_keys('Graphics Card')
 
-# Searching for the keyword within the Search Bar after signin in
-search_bar = driver.find_element(By.XPATH, '/html/body/div[1]/header/div/div[1]/div[2]/div/form/div[2]/div[1]/input')
-search_bar.send_keys()
+# CLicking the Search Button
+Search_Button_Click = driver.find_element(By.XPATH, '//*[@id="nav-search-submit-button"]')
+Search_Button_Click.click()
 
-
+time.sleep(999)
