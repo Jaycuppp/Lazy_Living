20e2a4c9bb87bf1d9fbcfc7cc5a829b822ccf9ee
Jakob <108171765+Jaycuppp@users.noreply.github.com>
2022-12-26 16:20:06 -0800

    Update Amazon-Product-Search.py

--- Amazon-Product-Search.py
+++ Amazon-Product-Search.py
@@ -10,7 +10,7 @@ path = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(path)
 driver.get('https://www.amazon.com/?tag=amazusnavi-20&hvadid=381823327672&hvpos=&hvnetw=g&hvrand=15369903253882284441&hvpone=&hvptwo=&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9031162&hvtargid=kwd-10573980&ref=pd_sl_7j18redljs_e&hydadcr=28883_11845445&gclid=CjwKCAjwmJeYBhAwEiwAXlg0ATpf1Z-JL2kpyEl7yRZRhGWjvKzG3tVkde1CAQCiNZ9JaYRHyJXjjRoCf5wQAvD_BwE')
 
-# GPU Keystroke Input
+
 Search_Button = driver.find_element(By.XPATH, '//*[@id="twotabsearchtextbox"]')
 # Change this to any product you want
 Search_Button.send_keys('Graphics Card')
