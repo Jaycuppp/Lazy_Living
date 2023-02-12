e84a7c4b6c31b2d5100aad891da4c75eeb7531b8
Hakob <hkeshis911@student.glendale.edu>
2023-02-12 15:27:14 -0800

    Delete Weather_Report.py

--- Automated-Web-Browsing/Weather_Report.py
+++
@@ -1,9 +0,0 @@
-import time
-from selenium import webdriver
-
-#Initial Setup
-path = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(path)
-driver.get('https://weather.com/weather/today/l/c3c69cfc9248e306c9c54533c6b25eca5c36122f5ebebc50c48908877ccfcb97')
-
-time.sleep(3600)
